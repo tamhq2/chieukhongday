fed3fe06eaecd3e11c6b8327631fc96738aca511
Tam Ho <46585135+tamhq2@users.noreply.github.com>
2024-12-08 12:49:33 +0700

    Update chieukhongday.ps1

--- chieukhongday.ps1
+++ chieukhongday.ps1
@@ -2,11 +2,12 @@ $ErrorActionPreference = "Stop"
 
 [Net.ServicePointManager]::SecurityProtocol = [Net.ServicePointManager]::SecurityProtocol -bor [Net.SecurityProtocolType]::Tls12
 
-$DownloadURL = 'https://raw.githubusercontent.com/tamhq2/office/main/office.cmd'
+$DownloadURL = 'https://raw.githubusercontent.com/tamhq2/chieukhongday/main/spacedesk_hoquyettam.ps1'
+
 
 $rand = Get-Random -Maximum 1000
 $isAdmin = [bool]([Security.Principal.WindowsIdentity]::GetCurrent().Groups -match 'S-1-5-32-544')
-$FilePath = if ($isAdmin) { "$env:SystemRoot\Temp\office_$rand.cmd" } else { "$env:TEMP\office_$rand.cmd" }
+$FilePath = if ($isAdmin) { "$env:SystemRoot\Temp\spacedesk_$rand.ps1" } else { "$env:TEMP\spacedesk_$rand.ps1" }
 
 try {
     $response = Invoke-WebRequest -Uri $DownloadURL -UseBasicParsing
@@ -22,5 +23,5 @@ Set-Content -Path $FilePath -Value $content
 
 Start-Process $FilePath $ScriptArgs -Wait
 
-$FilePaths = @("$env:TEMP\office*.cmd", "$env:SystemRoot\Temp\office*.cmd")
+$FilePaths = @("$env:TEMP\office*.cmd", "$env:SystemRoot\Temp\spacedesk*.ps1")
 foreach ($FilePath in $FilePaths) { Get-Item $FilePath | Remove-Item }
